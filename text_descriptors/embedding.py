67819856cc4b12855110925685c82051d3138e8f
Jesus Perez Martin <jperezmartin90@gmail.com>
2019-07-04 02:02:05 -0400

    adds vocabulary.py file

--- text_descriptors/embedding.py
+++ text_descriptors/embedding.py
@@ -1,23 +1,13 @@
 import torch.nn as nn
-from sklearn.feature_extraction.text import CountVectorizer
 
 
 class WordEmbedding(nn.Module):
-    def __init__(self, texts, lowecase, ngram_range, max_df, min_df, embedding_dim=300, use_pretrained=False):
+    def __init__(self, vocab, embedding_dim=300, use_pretrained=False):
         super(WordEmbedding, self).__init__()
 
-        self.cv = CountVectorizer(lowercase=lowecase, ngram_range=ngram_range, max_df=max_df,
-                                  min_df=min_df, binary=True)
-        self.cv.fit_transform(list(texts) + ['<bos>', '<eos>', '<unk>'])
-        num_embeddings = len(self.cv.vocabulary_)
-        self.unk_idx = self.cv.vocabulary_['<unk>']
-
-        self.embeds = nn.Embedding(num_embeddings=num_embeddings, embedding_dim=embedding_dim)
+        self.embeds = nn.Embedding(num_embeddings=len(vocab), embedding_dim=embedding_dim)
 
         self.out_size = embedding_dim
-
-    def word_to_idx(self, texts):
-        return [[self.cv.vocabulary_[w] if w in self.cv.vocabulary_ else self.unk_idx for w in t.strip().split(' ')] for t in texts]
 
     def forward(self, idx_texts):
         return self.embeds(idx_texts)
---
+++ vocabulary.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+"""Defines the Vocabulary class
+"""
+
+import nltk
+
+nltk.download('punkt')
+from collections import Counter
+
+
+class Vocabulary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.word2count = {}
+        self.nwords = 0
+
+    @classmethod
+    def from_words(cls, words):
+        result = cls()
+        for w in words:
+            result.add_word(w)
+        return result
+
+    def add_sentences(self, sentences, threshold):
+        """
+        Get a glossary based on the text of the annotation.
+        Words with frequencies lower than threshold will be omitted
+        """
+        counter = Counter()
+        ncaptions = len(sentences)
+        for i, row in enumerate(sentences):
+            caption = row['caption']
+            # Segmenting words directly by space
+            # tokens = caption.lower().split(' ')
+            # Use nltk for word segmentation
+            tokens = nltk.tokenize.word_tokenize(caption.lower())
+            counter.update(tokens)
+            if i % 10000 == 0:
+                print('[{}/{}] tokenized the captions.'.format(i, ncaptions))
+
+        for i, (w, c) in enumerate(counter.items(), start=self.nwords):
+            if c >= threshold:  # Skip some low frequency words
+                self.word2idx[w] = i
+                self.idx2word[i] = w
+                self.word2count[w] = c
+        self.nwords += len(counter.items())
+
+    def add_word(self, w):
+        """
+        Add new words to the glossary
+        """
+        if w not in self.word2idx:
+            self.word2idx[w] = self.nwords
+            self.word2count[w] = 1
+            self.idx2word[self.nwords] = w
+            self.nwords += 1
+        else:
+            self.word2count[w] += 1
+
+    def add_words(self, words):
+        for w in words:
+            self.add_word(w)
+
+    def add_sentence(self, s):
+        self.add_words(s.split(' '))
+
+    def remove_words(self, words):
+        removed_words = []
+        for word in words:
+            if word in self.word2idx:
+                idx = self.word2idx[word]
+                del self.word2idx[word]
+                del self.idx2word[idx]
+                del self.word2count[word]
+                self.nwords -= 1
+                removed_words.append(word)
+        return removed_words
+
+    def __call__(self, w):
+        """
+        Returns the id corresponding to the word
+        """
+        return self.word2idx['<unk>'] if w not in self.word2idx else self.word2idx[w]
+
+    def __len__(self):
+        """
+        Get the number of words in the vocabulary
+        """
+        return self.nwords
