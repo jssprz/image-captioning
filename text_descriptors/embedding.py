40e7a9ac4300c6e7b260544a4ab39c5245d4aba8
Jesus Perez Martin <jperezmartin90@gmail.com>
2019-07-04 01:13:06 -0400

    adds special tokens in WordEmbedding

--- text_descriptors/embedding.py
+++ text_descriptors/embedding.py
@@ -8,15 +8,16 @@ class WordEmbedding(nn.Module):
 
         self.cv = CountVectorizer(lowercase=lowecase, ngram_range=ngram_range, max_df=max_df,
                                   min_df=min_df, binary=True)
-        self.cv.fit_transform(texts)
+        self.cv.fit_transform(texts + ['<bos>', '<eos>', '<unk>'])
         num_embeddings = len(self.cv.vocabulary_)
+        self.unk_idx = self.cv.vocabulary_['<unk>']
 
         self.embeds = nn.Embedding(num_embeddings=num_embeddings, embedding_dim=embedding_dim)
 
         self.out_size = embedding_dim
 
     def word_to_idx(self, texts):
-        return [[self.cv.vocabulary_[w] for w in t.strip().split(' ')] for t in texts]
+        return [[self.cv.vocabulary_[w] if w in self.cv.vocabulary_ else self.unk_idx for w in t.strip().split(' ')] for t in texts]
 
     def forward(self, idx_texts):
         return self.embeds(idx_texts)
