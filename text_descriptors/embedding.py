210a624ce6b0689f82ad70e827b9ab161c3402e1
Jesus Perez Martin <jperezmartin90@gmail.com>
2019-07-04 02:03:11 -0400

    removes vocap arg from WordEmbedding __init__

--- text_descriptors/embedding.py
+++ text_descriptors/embedding.py
@@ -2,10 +2,10 @@ import torch.nn as nn
 
 
 class WordEmbedding(nn.Module):
-    def __init__(self, vocab, embedding_dim=300, use_pretrained=False):
+    def __init__(self, num_embeddings, embedding_dim=300, use_pretrained=False):
         super(WordEmbedding, self).__init__()
 
-        self.embeds = nn.Embedding(num_embeddings=len(vocab), embedding_dim=embedding_dim)
+        self.embeds = nn.Embedding(num_embeddings=num_embeddings, embedding_dim=embedding_dim)
 
         self.out_size = embedding_dim
 
